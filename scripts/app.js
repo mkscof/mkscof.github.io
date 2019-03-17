8079b3b1f5192e791f91b17224e52a3ad8787ad4
Morgan Scofield <mkscofie@gmail.com>
2019-03-16 17:50:54 -0700

    Fixed dropdown

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,3 @@
-var controller = (function() {
-    // var setupEventListeners = function() {
-    //     document.getElementById('githubBtn').addEventListener('click', window.location.href="https://github.com/mkscof");
-    //     document.getElementById('linkedinBtn').addEventListener('click', window.location.href="https://www.linkedin.com/in/mkscof/");
-    // };
-
-    return {
-        init: function(){
-            console.log('Hello Again!');
-            // setupEventListeners();
-        }
-    };
-})();
-
-controller.init();
+$(document).ready(function() {
+    $('.dropdown-toggle').dropdown();
+});
